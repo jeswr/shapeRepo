a653a4b0cecf4ae2a00d75448d038f847fa2e645
Jesse Wright <63333554+jeswr@users.noreply.github.com>
2023-05-19 17:42:02 +0200

    chore: upgrade deps (#3)
    * chore: upgrade deps
    
    * chore: enable ldp:contains

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-const app = require('rdf-serve').default(require('path').join(__dirname, 'shapes'));
+const app = require('rdf-serve').default(require('path').join(__dirname, 'shapes'), true);
 module.exports = app.listen(process.env.PORT || 3000);
